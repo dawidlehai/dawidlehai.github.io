8fa4810184694e6b33a0f0bb640881aaf896f04f
saitamason <92246829+saitamason@users.noreply.github.com>
2024-05-22 19:48:41 +0200

    perf: optimize loadFonts function with sessionStorage

--- load-fonts.js
+++ load-fonts.js
@@ -1,10 +1,12 @@
 "use strict";
 (async function loadFonts() {
-  if ("fonts" in document) {
+  const fontsLoaded = sessionStorage.getItem("fontsLoaded");
+  if (!fontsLoaded && "fonts" in document) {
     await Promise.all([
       document.fonts.load("1rem JetBrains Mono"),
       document.fonts.load("italic 1rem JetBrains Mono"),
     ]);
-    document.documentElement.classList.add("fonts-loaded");
+    sessionStorage.setItem("fontsLoaded", "true");
   }
+  document.documentElement.classList.add("fonts-loaded");
 })();
