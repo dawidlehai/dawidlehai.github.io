1cc54b9e8174d20c5d7ad97fb6f54057e202b6d4
saitamason <92246829+saitamason@users.noreply.github.com>
2024-12-09 11:59:37 +0100

    refactor: refactor attachVideoPlayButtons

--- scripts/attachVideoPlayButtons.js
+++ scripts/attachVideoPlayButtons.js
@@ -1,24 +1,31 @@
 function attachVideoPlayButtons() {
+  const playText = "Play";
+  const pauseText = "Pause";
+  const buttonClass = "video-play-button";
   const videoContainers = document.querySelectorAll(".js-videos");
 
-  videoContainers.forEach((container) => {
+  videoContainers.forEach(function attachPlayButton(container) {
     const desktopVideo = container.querySelector(".js-video-desktop");
     const mobileVideo = container.querySelector(".js-video-mobile");
 
     if (desktopVideo && mobileVideo) {
       const button = document.createElement("button");
-      button.innerText = "Play";
+      button.classList.add(buttonClass);
+      button.innerText = playText;
 
-      button.addEventListener("click", () => {
+      function handleVideoButtonClick() {
         if (desktopVideo.paused) {
           desktopVideo.play();
           mobileVideo.play();
+          button.innerText = pauseText;
         } else {
           desktopVideo.pause();
           mobileVideo.pause();
+          button.innerText = playText;
         }
-      });
+      }
 
+      button.addEventListener("click", handleVideoButtonClick);
       container.appendChild(button);
     }
   });
